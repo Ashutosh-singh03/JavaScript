0493fb4a2a93848c7b7564fb1c0a8119923821e2
Ashutosh-singh03 <singhashutosh1025@gmail.com>
2024-08-05 10:01:15 +0000

    dataTypes

---
+++ basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict";// treat all JS code as newer version
+
+// alert(3+3) we are using node.js not browser
+let age=23
+let name="amit"
+let adult=true
+
+// data types
+// number=> 2 to power 53
+// bigint
+// string=""
+// boolean=>true/false
+// null=> standalone value
+// undefined
+// symbol=>uniquenes
+
+
+// object
+
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
