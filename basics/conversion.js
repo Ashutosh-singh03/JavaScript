446458409f0f30df9f03dbe1feaaa69c9cc7a161
Ashutosh-singh03 <singhashutosh1025@gmail.com>
2024-08-06 12:39:33 +0000

    conversion

---
+++ basics/conversion.js
@@ -0,0 +1,29 @@
+// let score="33"
+let score1="abc"
+// let score2=null
+// let score3=undefined
+
+ 
+
+
+// console.log(typeof score)
+console.log(typeof score1)
+// console.log(typeof score2)
+// console.log(typeof score3)
+
+// let valueInNumber=Number(score)
+let valueInNumber1=Number(score1)
+// let valueInNumber2=Number(score2)
+// let valueInNumber3=Number(score3)
+// console.log(typeof valueInNumber)
+console.log(typeof valueInNumber1)
+// console.log(typeof valueInNumber2)
+// console.log(typeof valueInNumber3)
+
+
+let number=33
+let number1=String(number)
+console.log(number1)
+console.log(typeof number1)
+
+
