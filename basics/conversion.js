922261c675b791bd0c731b376a1eb381d37639f9
Ashutosh-singh03 <singhashutosh1025@gmail.com>
2024-08-06 13:09:00 +0000

    operator

--- basics/conversion.js
+++ basics/conversion.js
@@ -7,23 +7,52 @@ let score1="abc"
 
 
 // console.log(typeof score)
-console.log(typeof score1)
+// console.log(typeof score1)
 // console.log(typeof score2)
 // console.log(typeof score3)
 
 // let valueInNumber=Number(score)
-let valueInNumber1=Number(score1)
+// let valueInNumber1=Number(score1)
 // let valueInNumber2=Number(score2)
 // let valueInNumber3=Number(score3)
 // console.log(typeof valueInNumber)
-console.log(typeof valueInNumber1)
+// console.log(typeof valueInNumber1)
 // console.log(typeof valueInNumber2)
 // console.log(typeof valueInNumber3)
 
 
-let number=33
-let number1=String(number)
-console.log(number1)
-console.log(typeof number1)
+// let number=33
+// let number1=String(number)
+// console.log(number1)
+// console.log(typeof number1)
+
+// ******************** operations ****************************
+
+let value=3
+let negValue=-value
+// console.log(negValue)
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**2)
+// console.log(2%2)
+
+
+let str1="ashutosh"
+let str2=" Singh"
+let str3=str1+str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+let gameCounter=100
+++gameCounter
+console.log(gameCounter)
 
 
