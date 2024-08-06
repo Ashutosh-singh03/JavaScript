612e27eae9982bd860b993089e81471cb909b48a
Ashutosh-singh03 <singhashutosh1025@gmail.com>
2024-08-06 14:47:39 +0000

    comparison and datatypes

---
+++ basics/05_datatypesSummary.js
@@ -0,0 +1,30 @@
+//primitive 
+
+// 7 types=> string number boolean undefined null BigInt symbol
+
+const score=100
+const scoreValue=100.3
+const isLofggedIn=true
+const temperature=null //it means not decided right now 
+let userEmail
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+// console.log(id===anotherId)
+
+
+
+// refrence (Non-primitive)
+
+// Array, objects ,Functions
+
+const heroes=["shaktiman","doga","loki"];
+let marks={
+    name:"Ashu" ,
+    age:20
+
+}
+
+const myfunction=function(){
+    console.log("hello world");
+}
