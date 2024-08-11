a9a9fb8566dbf4f956e301dd115c880e1ef98a84
Ashutosh-singh03 <singhashutosh1025@gmail.com>
2024-08-11 13:43:05 +0000

    array 1 is done

---
+++ 02_basics/01_array.js
@@ -0,0 +1,34 @@
+
+const arr=[1,2,3,4,5]
+console.log(arr)
+
+// arrray method
+
+// arr.push(6)
+
+// arr.push(7)
+
+// arr.pop()
+
+// arr.unshift(9)//it adds number at starting
+// arr.shift()// it deletes the first element
+// console.log(arr)
+
+// console.log(arr.includes(9))// it checks if the number is present in the array or not
+
+// console.log(arr.indexOf(4))
+
+// const newarr=arr.join()//it changes its type
+// console.log(newarr)
+
+const myarr1=arr.slice(1,3)
+console.log("A", myarr1)
+const myarr2=arr.splice(1,3)// it changes the original array
+console.log("B",myarr2)
+console.log("C",arr)
+
+
+
+const myHeroes=["loki","ironman"]
+const arr1=new Array(1,2,3,4)//declare a array
+// console.log(arr1)
